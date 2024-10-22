010bed3932fcc858dca6dcbc6c77784b48dd2615
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-10-22 18:33:18 +0400

    Initial Commit

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanbeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            bool result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
